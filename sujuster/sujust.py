ccb687db405bd9ab36201bb5c65d6820fd31d470
Axel Bock <mr.axel.bock@gmail.com>
2016-08-12 23:38:19 +0200

    add & update code docs

--- sujuster/sujust.py
+++ sujuster/sujust.py
@@ -2,10 +2,16 @@ from argparse import ArgumentParser
 from datetime import datetime, timedelta
 from sys import stdout
 
+
 config = None
 
 
 def parse_args():
+    """
+    Parses the argument list.
+
+    :return: None
+    """
     global config
     parser = ArgumentParser()
     parser.add_argument("srt_file",
@@ -28,8 +34,11 @@ def get_subtitles(infile):
     """
     Generator which returns subtitles from a given already opened input file.
 
+    Yields a list which is basically all lines between empty lines in the
+    subtitles file.
+
     :param infile: The opened file-handle (or file-like object)
-    :return: A generator
+    :return: Yields [line1, line2, ...] of each subtitle block
     """
     line = "start"
     while line:
@@ -44,6 +53,15 @@ def get_subtitles(infile):
 
 
 def adjust_subtitle(st, td):
+    """
+    Gets a list representing a subtitle. It expects the 'classic' .srt format
+    with hours in the time stamps as the 2nd element of the list. Will then
+    adjust the start and stop time by the given time delta object.
+
+    :param st: The list containing the subtitle
+    :param td: The timedelta object which is used to adjust
+    :return: The processed list, although it is modified in-place.
+    """
     # we only know microseconds, though the timestamp has milliseconds ...
     markers = [x + "000" for x in st[1].split(" --> ")]
     marker_objs = [datetime.strptime(x, "%H:%M:%S,%f") + td for x in markers]
@@ -55,8 +73,14 @@ def adjust_subtitle(st, td):
 
 
 def adjust_subtitles(outfile):
-    # see http://stackoverflow.com/a/2459793/902327
+    """
+    Opens config.srt_file as subtitle file, then iterates thorugh all subtitles
+    and adjusts the start and stop times using the adjust_subtitle function.
+    :param outfile: The output file handle (or any file-like object)
+    :return: None
+    """
     td = timedelta(seconds=config.offset)
+    # see http://stackoverflow.com/a/2459793/902327
     with open(config.srt_file, "r", encoding='utf-8-sig') as infile:
         for num, st in enumerate(get_subtitles(infile)):
             adjust_subtitle(st, td)
@@ -65,6 +89,10 @@ def adjust_subtitles(outfile):
 
 
 def start():
+    """
+    The starting point of everything.
+    :return: None
+    """
     parse_args()
     if config.outfile:
         with open(config.outfile, "w", encoding="utf-8") as outfile:
