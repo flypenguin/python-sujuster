cf6baa6e651d86d2a85120ef61c5d023bbc3e8b2
Axel Bock <mr.axel.bock@gmail.com>
2016-08-12 23:10:43 +0200

    v1.0.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,8 @@
-1.0.1
------
+1.0.2 & 1.0.1
+-------------
 
 - change executable name to "sujust" as intended originally
+- took more than one try. it's late.
 
 
 1.0.0
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pip.download import PipSession
 import io
 
 
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 
 
 install_reqs = parse_requirements("./requirements.txt", session=PipSession())
